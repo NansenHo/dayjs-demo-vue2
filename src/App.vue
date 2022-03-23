46f6567fecd131b92954d1f6d2c5e5d0adf25439
nansen <nansen.ho@foxmail.com>
2022-03-23 15:34:28 +0800

    dayjs init

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,27 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{ a }}</p>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import dayjs from "dayjs"
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      a: ""
+    }
+  },
+  mounted(){
+    console.log(dayjs("2022-3-23 07:00").tz(dayjs.tz.guess()).format('YYYY-MM-DD HH:mm:ss'), dayjs.tz.guess())
+    this.a = this.$dayjs().format("YY||DD||MM")
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import dayjs from "dayjs"
+let utc = require('dayjs/plugin/utc')
+let timezone = require('dayjs/plugin/timezone')
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 Vue.config.productionTip = false
+Vue.use(dayjs)
+Vue.prototype.$dayjs = dayjs
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+let local = dayjs.tz.guess()
+console.log(local)
+console.log(dayjs("2022-3-23 07:00").tz(local).format('YYYY-MM-DD HH:mm:ss'), dayjs.tz.guess())
+
+console.log(dayjs("2014-06-01 12:00").tz("America/New_York").format())
+console.log(dayjs("2014-06-01 12:00").tz("America/New_York", true).format())
+
+console.log(dayjs.tz.guess())
